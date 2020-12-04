3c05dce3dc37e2ca4ea20d2ead30e8eb669f90c7
Koslun <adam@normative.io>
2020-12-04 19:22:20 +0100

    style(day4): auto-format with prettier

--- src/day4/day4.js
+++ src/day4/day4.js
@@ -1,17 +1,17 @@
-var fs = require("fs");
+var fs = require('fs');
 
-const text = fs.readFileSync("./2020_day4_input.txt", "utf-8");
+const text = fs.readFileSync('./2020_day4_input.txt', 'utf-8');
 // const text = fs.readFileSync("./2020_day4_input.mock.txt", "utf-8");
 
-const textByLine = text.split("\n\n");
+const textByLine = text.split('\n\n');
 
 console.log(
   `Parsed ${textByLine.length} numbers. O(n) = ${textByLine.length}, O(n^2) = ${
     textByLine.length * textByLine.length
-  }, O(n^3) = ${textByLine.length * textByLine.length * textByLine.length}`
+  }, O(n^3) = ${textByLine.length * textByLine.length * textByLine.length}`,
 );
 
-console.log("Number of passports: ", textByLine.length);
+console.log('Number of passports: ', textByLine.length);
 
 const passports = segmentPassports(textByLine);
 
@@ -24,15 +24,15 @@ validList.forEach((valid) => {
   }
 });
 
-console.log("Valid passports", validCount);
+console.log('Valid passports', validCount);
 
 function segmentPassports(unformattedPassports) {
   const passports = [];
   for (let i = 0; i < unformattedPassports.length; i++) {
-    const squashedPassportList = unformattedPassports[i].split("\n");
-    let squashedPassport = "";
+    const squashedPassportList = unformattedPassports[i].split('\n');
+    let squashedPassport = '';
     for (let j = 0; j < squashedPassportList.length; j++) {
-      squashedPassport = squashedPassport.concat(squashedPassportList[j], " ");
+      squashedPassport = squashedPassport.concat(squashedPassportList[j], ' ');
     }
     passports.push(squashedPassport);
   }
@@ -41,47 +41,47 @@ function segmentPassports(unformattedPassports) {
 
 function isValidPassport(passport) {
   // console.log("passport ", passport);
-  const fields = passport.trim().split(" ");
-  const requiredFields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
-  const optionalFields = ["cid"];
+  const fields = passport.trim().split(' ');
+  const requiredFields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'];
+  const optionalFields = ['cid'];
   let noInvalidKey = true;
   let requiredFieldCount = 0;
   for (let i = 0; i < fields.length; i++) {
-    const [key, value] = fields[i].split(":");
+    const [key, value] = fields[i].split(':');
     // console.log("key", key);
     //  || optionalFields.includes(key)
     if (requiredFields.includes(key)) {
       let validKey = true;
-      if ("byr" === key) {
+      if ('byr' === key) {
         let numValue = Number.parseInt(value);
         validKey = numValue >= 1920 && numValue <= 2002;
-      } else if ("iyr" === key) {
+      } else if ('iyr' === key) {
         let numValue = Number.parseInt(value);
         validKey = numValue >= 2010 && numValue <= 2020;
-      } else if ("eyr" === key) {
+      } else if ('eyr' === key) {
         let numValue = Number.parseInt(value);
         validKey = numValue >= 2020 && numValue <= 2030;
-      } else if ("hgt" === key) {
+      } else if ('hgt' === key) {
         let numStr = value.substring(0, value.length - 2);
         let numValue = Number.parseInt(numStr);
-        if (value.endsWith("cm")) {
+        if (value.endsWith('cm')) {
           validKey = numValue >= 150 && numValue <= 193;
-        } else if (value.endsWith("in")) {
+        } else if (value.endsWith('in')) {
           validKey = numValue >= 59 && numValue <= 76;
         } else {
           validKey = false;
         }
-      } else if ("hcl" === key) {
+      } else if ('hcl' === key) {
         const hexValue = Number.parseInt(value.substring(1, value.length), 16);
         validKey =
           value.length === 7 &&
-          value.startsWith("#") &&
+          value.startsWith('#') &&
           !Number.isNaN(hexValue);
-      } else if ("ecl" === key) {
-        const eyeColors = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"];
+      } else if ('ecl' === key) {
+        const eyeColors = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'];
         validKey = eyeColors.includes(value);
-      } else if ("pid" === key) {
-        const numValueStr = value.replace(/^(-)?0+(0\.|\d)/, "$1$2");
+      } else if ('pid' === key) {
+        const numValueStr = value.replace(/^(-)?0+(0\.|\d)/, '$1$2');
         const numValue = Number.parseInt(numValueStr);
         validKey = value.length === 9 && !Number.isNaN(numValue);
       }
